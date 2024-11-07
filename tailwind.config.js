5b469b3fc8007a5e1937e64b9e5dc1a11f243243
moriroKim <moriroKim@gmail.com>
2024-11-07 18:43:07 +0900

    Design: tailwind config 테마설정 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,14 @@ export default {
       colors: {
         primary: "#000000", // 메인 브랜드 색상
         secondary: "#ffffff", // 배경에 사용하는 중립 색상
-        accent: "##F77830", // 포인트 색상
-        text: "#000000", // 기본 텍스트 색상 (짙은 회색)
-        textSub: "#7F7F7F", // 보조 텍스트 색상 (연한 회색)
-        textLink: '#0000FF', // 링크
-        textGray: '#C7C7C7',
+        accent: "#F77830", // 포인트 색상
+        gray100: "#F1F1F1",
+        gray300: "#CBCAC7",
+        gray500: "#B2B2B2",
+        gray700: "#7F7F7F",
+        blue700: '#0000FF', // 링크
         success: '#008541',
         error: '#FF6E66',
-        border: '#D9D9D9',
-        borderGray: '#7F7F7F',
-        boxGray: '#F6F6F6',
-        boxDarkGray: '#3C3C3C',
       },
       fontFamily: {
         sans: ["Noto Sans KR", "Arial", "sans-serif"],
