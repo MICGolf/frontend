655f01ea1621952ec1819421ac66f180999c35db
shin-minhyuk <afs_style@naver.com>
2024-11-08 12:13:17 +0900

    feat: 슬라이더 추가 수정예정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import imageSection1 from '@/assets/imgs/image_section-1.jpg';
+import MultipleItems from './components/MultipleItems';
 
 const HomePage = () => {
   return (
@@ -11,16 +12,15 @@ const HomePage = () => {
             backgroundSize: 'cover',
           }}
         ></div>
-        <div className='absolute z-20 mt-[100vh] h-[400dvh] w-full bg-white'></div>
-      </section>
+        <div className='absolute z-20 mt-[100vh] h-[100vh] w-full bg-white'></div>
 
-      {/* 컨텐츠 섹션 */}
-      <section className='mt-screen relative px-[130px]'>
-        <h1>Best Product</h1>
-        <div>
-          <div></div>
+        {/* 컨텐츠 섹션 */}
+        <div className='mt-screen px-[130px]'>
+          <h1>Best Product</h1>
+          <div>
+            <MultipleItems />
+          </div>
         </div>
-        <div></div>
       </section>
     </>
   );
