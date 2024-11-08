ac0d6888e0859e47d15c10d2e7f7a50147f5075a
shin-minhyuk <afs_style@naver.com>
2024-11-08 14:54:16 +0900

    ✅ feat: 홈 캐러셀 추가

--- src/pages/home/components/MultipleItems.tsx
+++ src/pages/home/components/MultipleItems.tsx
@@ -1,48 +1,51 @@
+import { useState, useRef } from 'react';
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
 
-const MultipleItems = () => {
+export default function MultipleItems() {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const sliderRef = useRef<Slider>(null); // Slider 레퍼런스 생성
+  const pagePerSlide = 4;
+  const slideLength = 19;
+  const totalSlides = Math.ceil(slideLength / pagePerSlide); // Assuming 100/4 (slidesToScroll) = 25 total segments
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: pagePerSlide,
+    slidesToScroll: pagePerSlide,
+    beforeChange: (_current: number, next: number) => setActiveSlide(next / 4),
+    appendDots: () => (
+      <div className='relative w-full'>
+        <div className='flex w-full justify-between'>
+          {Array.from({ length: totalSlides }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => sliderRef.current?.slickGoTo(index * 4)} // 특정 슬라이드로 이동
+              className={`relative my-6 h-1 w-full transition-all duration-300 ${index === activeSlide ? 'bg-black' : 'bg-gray300'}`}
+            />
+          ))}
+        </div>
+      </div>
+    ),
+    customPaging: () => <div className='hidden' />, // Hide default dots but keep functionality
   };
+
   return (
-    <div className='slider-container'>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
-      </Slider>
+    <div className='mx-auto w-full'>
+      <div className='slider-container'>
+        <Slider ref={sliderRef} {...settings}>
+          {Array.from({ length: slideLength }).map((_, index) => (
+            <div key={index}>
+              <div className='flex h-[465px] items-center justify-center rounded-lg bg-gray-200'>
+                <h3 className='text-xl font-semibold'>{index}</h3>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
-};
-
-export default MultipleItems;
+}
