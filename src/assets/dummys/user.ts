0cdf8544dad9106ec59bb6668120708b05c8664e
shin-minhyuk <afs_style@naver.com>
2024-11-11 17:52:31 +0900

    design: 로그인 성공 페이지 ui 구현

---
+++ src/assets/dummys/user.ts
@@ -0,0 +1,6 @@
+import { UserData } from './types';
+
+export const user: UserData = {
+  name: '신미낙',
+  email: 'afs_style@naver.com',
+};
--- src/pages/auth/SignUpCompletePage.tsx
+++ src/pages/auth/SignUpCompletePage.tsx
@@ -1,5 +1,17 @@
+import { user } from '@/assets/dummys/user';
+import signupCompleteCheck from '@/assets/icons/signupCompleteCheck.svg';
+
 const SignUpCompletePage = () => {
-  return <div>SignUpCompletePage</div>;
+  return (
+    <div className='flex flex-col items-center justify-center gap-4 pt-[250px]'>
+      <img src={signupCompleteCheck} alt='로그인 성공 체크표시' />
+      <div className='text-5xl font-[700]'>회원 가입이 완료 되었습니다.</div>
+      <div className='flex flex-col items-center'>
+        <p className='text-2xl'>{user.name}님의 회원가입을 축하합니다.</p>
+        <p className='text-2xl'>지금 바로 믹골프 회원만의 다양한 혜택을 받아보세요!</p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpCompletePage;
