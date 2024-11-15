3d0aff4169c06c337eee16b80ef2af903fd08aa4
moriroKim <moriroKim@gmail.com>
2024-11-15 18:14:26 +0900

    feat: 품절시에 장바구니 추가, 구매불가 기능 추가

---
+++ src/hooks/useSaleState.ts
@@ -0,0 +1,35 @@
+import { ProductDetail } from '@/assets/dummys/types';
+import { useEffect, useState } from 'react';
+
+const useSaleState = (product: ProductDetail) => {
+  const [isSale, setIsSale] = useState<boolean>(true);
+  const { sale } = product;
+  const { unit, value } = sale;
+
+  useEffect(() => {
+    setIsSale(sale.is_active);
+  }, [sale.is_active]);
+
+  const renderPriceTextByUnit = (unit: string, value: number) => {
+    switch (unit) {
+      case '%':
+        return `${value}${unit} 할인`;
+      case '-':
+        return `${value}원 할인`;
+      default:
+        return '';
+    }
+  };
+
+  const saleLabelText = renderPriceTextByUnit(unit, value);
+
+  // 할인 라벨 조건부 스타일
+  const labelClassNames =
+    unit === '%'
+      ? 'bg-primary text-white' // %일 경우 배경색
+      : 'border border-primary text-primary'; // -일 경우 테두리 색과 글자색
+
+  return { isSale, saleLabelText, labelClassNames };
+};
+
+export default useSaleState;
--- src/pages/shop/detailPage/components/ProductDetailView.tsx
+++ src/pages/shop/detailPage/components/ProductDetailView.tsx
@@ -6,6 +6,9 @@ import SizeBtns from './SizeBtns';
 import Counter from './CounterBtn';
 import { ProductDetailViewProps } from '../types';
 import { Color, Size } from '@/assets/dummys/types';
+import useSoldOutState from '@/hooks/useSoldoutState';
+import useSaleState from '@/hooks/useSaleState';
+import SaleLabel from '../../components/SaleLabel';
 
 const ProductDetailView = ({ data }: ProductDetailViewProps) => {
   const [detailImage, setDetailImage] = useState<string[]>(data.colors[0]?.images);
@@ -13,6 +16,8 @@ const ProductDetailView = ({ data }: ProductDetailViewProps) => {
   const [selectedSize, setSelectedSize] = useState<Size | null>(null);
   const [count, setCount] = useState<number>(1);
   const [maxCount, setMaxCount] = useState<number>(1);
+  const { isSoldOut } = useSoldOutState(data);
+  const { isSale, saleLabelText, labelClassNames } = useSaleState(data);
 
   return (
     <section className='flex flex-col min-h-screen transition-all duration-300 ease-in-out xl:flex-row'>
@@ -27,8 +32,21 @@ const ProductDetailView = ({ data }: ProductDetailViewProps) => {
       <div className='z-[1] flex w-full flex-col overflow-auto px-[50px] pb-[50px] pt-[150px] transition-all duration-300 ease-in-out xl:sticky xl:top-0 xl:h-[calc(100vh)] xl:w-1/2 xl:border-l xl:border-primary'>
         <div className='flex flex-col h-full gap-8'>
           <div className='flex flex-col gap-4'>
-            <h2 className='text-4xl font-bold transition-transform duration-300 ease-in-out'>{data.name}</h2>
-            <p className='text-2xl font-light'>₩{data.price.toLocaleString()}</p>
+            <div className='flex items-center gap-2'>
+              <h2 className='text-4xl font-bold transition-transform duration-300 ease-in-out'>{data.name}</h2>
+              <SaleLabel classString={labelClassNames} text={saleLabelText} />
+            </div>
+            {isSale ? (
+              <div>
+                <p className='text-xl font-light line-through text-gray700'>₩{data.price.toLocaleString()}</p>
+                <p className='text-2xl font-light'>₩{data.sale.result.toLocaleString()}</p>
+              </div>
+            ) : (
+              <div>
+                <p className='text-2xl font-light'>₩{data.price.toLocaleString()}</p>
+                <p className='text-2xl font-light'>₩{data.sale.result.toLocaleString()}</p>
+              </div>
+            )}
           </div>
 
           <div className='flex flex-col gap-6 mt-auto'>
@@ -41,8 +59,14 @@ const ProductDetailView = ({ data }: ProductDetailViewProps) => {
               setMaxCount={setMaxCount}
               selectedSize={selectedSize}
               selectedColor={selectedColor}
+              isSoldOut={isSoldOut}
             />
-            <div className='flex flex-col gap-4 transition-all duration-300 ease-in-out xl:flex'>
+            <div className='relative flex flex-col gap-4 transition-all duration-300 ease-in-out xl:flex'>
+              {isSoldOut && (
+                <div className='absolute z-50 flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.45)] text-2xl text-white'>
+                  <span className='absolute animate-pulse'>Sold Out</span>
+                </div>
+              )}
               <AddCartBtn />
               <NaverPayBtn />
             </div>
