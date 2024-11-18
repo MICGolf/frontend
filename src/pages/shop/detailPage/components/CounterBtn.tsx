22f57aed935e3a5ba55f190af8bfbac1afbdf30a
moriroKim <moriroKim@gmail.com>
2024-11-18 09:14:44 +0900

    design: 카운터버튼 애니메이션 및 메세지UI 추가

--- src/pages/shop/detailPage/components/CounterBtn.tsx
+++ src/pages/shop/detailPage/components/CounterBtn.tsx
@@ -1,7 +1,9 @@
 import minus from '@/assets/icons/minus.svg';
 import plus from '@/assets/icons/plus.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CounterBtnProps } from '../types';
+import { AnimatePresence, motion } from 'framer-motion';
+import CounterMessage from './CounterMessage';
 
 const CounterBtn = ({
   count,
@@ -12,6 +14,16 @@ const CounterBtn = ({
   selectedColor,
   isSoldOut,
 }: CounterBtnProps) => {
+  const [isMaxStock, setIsMaxStock] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (count === maxCount) {
+      setIsMaxStock(true);
+    } else {
+      setIsMaxStock(false);
+    }
+  }, [count, maxCount]);
+
   useEffect(() => {
     setCount(1);
     if (selectedSize) {
@@ -33,39 +45,45 @@ const CounterBtn = ({
   };
 
   return (
-    <div className='relative flex flex-col justify-center w-full gap-2 md:justify-start'>
+    <div className='relative flex w-full flex-col justify-center gap-2 md:justify-start'>
       <h3 className='text-lg font-light md:text-2xl'>수량</h3>
       {selectedColor && selectedSize ? (
         <div className='flex w-full'>
-          <div
-            className='item-center flex h-[40px] w-[130px] justify-around border border-gray200'
-            aria-live='polite'
-            role='group'
-            aria-labelledby='quantity-selection'
-          >
+          <div className='relative flex h-[40px] w-[130px] items-center justify-around border border-gray200'>
             <button
               type='button'
-              className='flex items-center justify-center flex-1'
+              className='flex h-full w-full flex-1 items-center justify-center transition-all duration-300 hover:bg-gray-100 active:bg-gray-100'
               aria-label='수량 감소'
               onClick={handleDecrease}
             >
               <img src={minus} alt='수량 감소 버튼' />
             </button>
-            <span
-              id='quantity-selection'
-              className='text-item flex flex-1 items-center justify-center text-[16px] font-thin'
-              aria-live='assertive'
-            >
-              {isSoldOut ? 0 : count}
-            </span>
+
+            <p className='relative flex h-[24px] flex-1 items-center justify-center overflow-hidden text-[16px] font-thin'>
+              <AnimatePresence mode='popLayout'>
+                <motion.span
+                  key={count}
+                  initial={{ y: -20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: 20, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className={`absolute cursor-default bg-transparent`}
+                >
+                  {isSoldOut ? 0 : count}
+                </motion.span>
+              </AnimatePresence>
+            </p>
+
             <button
               type='button'
-              className='flex items-center justify-center flex-1'
+              className='flex h-full w-full flex-1 items-center justify-center transition-all duration-300 hover:bg-gray-100 active:bg-gray-100'
               aria-label='수량 증가'
               onClick={handleIncrease}
             >
               <img src={plus} alt='수량 증가 버튼' />
             </button>
+
+            <CounterMessage isOpen={isMaxStock} />
           </div>
         </div>
       ) : (
