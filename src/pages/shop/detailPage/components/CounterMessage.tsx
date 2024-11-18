062f9e46a9f9d385a37dce18b60431cdc1ee715d
moriroKim <moriroKim@gmail.com>
2024-11-18 09:14:44 +0900

    design: 카운터버튼 애니메이션 및 메세지UI 추가

---
+++ src/pages/shop/detailPage/components/CounterMessage.tsx
@@ -0,0 +1,41 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
+const CounterMessage = ({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <AnimatePresence mode='wait'>
+      {isOpen && (
+        <motion.span
+          key='message'
+          initial={{ opacity: 0, scale: 0.8, x: 40 }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            x: 0,
+            transition: {
+              type: 'spring',
+              damping: 18,
+              stiffness: 300,
+              duration: 0.3,
+            },
+          }}
+          exit={{
+            opacity: 0,
+            scale: 0.95,
+            x: 20,
+            transition: {
+              type: 'spring',
+              damping: 18,
+              stiffness: 300,
+              duration: 0.3,
+            },
+          }}
+          className='absolute -right-[180px] bg-error px-2 py-1 text-center text-[12px] text-secondary'
+        >
+          구매가능한 최대수량입니다.
+        </motion.span>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default CounterMessage;
