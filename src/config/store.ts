111f20958faffc461861a50ca77840936e24581b
moriroKim <moriroKim@gmail.com>
2024-11-18 09:13:58 +0900

    chore: type 정의 코드 분리

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,31 +1,10 @@
 import { create } from 'zustand';
-
-interface HeaderRefStore {
-  headerRef: HTMLElement | null;
-  setHeaderRef: (headerRef: HTMLElement | null) => void;
-}
+import { HeaderRefStore, MobileStore, User, UserStore } from './types';
 
 export const useHeaderStore = create<HeaderRefStore>((set) => ({
   headerRef: null,
   setHeaderRef: (headerRef) => set({ headerRef }),
 }));
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  cartItems: any[];
-}
-
-interface UserStore {
-  user: User | null; // FIX: user type 정의
-  setUser: (user: User) => void; // FIX: user type 정의
-}
-
-interface MobileStore {
-  isMobileMode: boolean;
-  setIsMobileMode: (state: boolean) => void;
-}
 
 export const useUserStore = create<UserStore>(() => ({
   user: null,
