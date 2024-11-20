79fe46fa60fddb013a22be8c695ad761aca3a23a
shin-minhyuk <afs_style@naver.com>
2024-11-20 12:22:37 +0900

    fix: chart type register error fixed

--- src/pages/admin/main/components/SaleGraph.tsx
+++ src/pages/admin/main/components/SaleGraph.tsx
@@ -1,31 +1,18 @@
 import { SectionBox } from '../../components/SectionBox';
-import { Chart } from 'react-chartjs-2';
-import {
-  ChartData,
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+import { Line } from 'react-chartjs-2';
+import { ChartData } from 'chart.js';
+import './chartSetup';
 
 const data: ChartData<'line'> = {
   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
   datasets: [
     {
-      type: 'line',
       label: '결제건수',
       borderColor: 'rgb(54, 162, 235)',
       borderWidth: 2,
       data: [500, 300, 800, 800, 300, 200, 100, 600, 500, 300],
     },
     {
-      type: 'line',
       label: '결제금액',
       borderColor: 'rgb(228, 74, 22)',
       borderWidth: 2,
@@ -37,7 +24,7 @@ const data: ChartData<'line'> = {
 export const SaleGraph = () => {
   return (
     <SectionBox title='매출통계'>
-      <Chart type='line' data={data} />
+      <Line data={data} />
     </SectionBox>
   );
 };
--- src/pages/admin/main/components/UserConnection.tsx
+++ src/pages/admin/main/components/UserConnection.tsx
@@ -1,24 +1,12 @@
 import { SectionBox } from '../../components/SectionBox';
-import { Chart } from 'react-chartjs-2';
-import {
-  ChartData,
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+import { ChartData } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import './chartSetup';
 
 const data: ChartData<'line'> = {
   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
   datasets: [
     {
-      type: 'line',
       label: '어제기준',
       borderColor: 'rgb(111, 233, 24)',
       borderWidth: 2,
@@ -29,7 +17,7 @@ const data: ChartData<'line'> = {
 export const UserConnection = () => {
   return (
     <SectionBox title='고객유입'>
-      <Chart type='line' data={data} />
+      <Line data={data} />
     </SectionBox>
   );
 };
