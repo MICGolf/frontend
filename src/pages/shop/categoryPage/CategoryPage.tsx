8b787b217446016daeaeab09b618c76c29dfb2e5
moriroKim <moriroKim@gmail.com>
2024-11-26 16:36:45 +0900

    feat: Shop/Category페이지 무한스크롤 기능 추가

--- src/pages/shop/categoryPage/CategoryPage.tsx
+++ src/pages/shop/categoryPage/CategoryPage.tsx
@@ -1,15 +1,21 @@
-import { useParams } from 'react-router-dom';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 import ProductCard from '../components/ProductCard';
 import SortDropdown from '../components/SortDropdown';
 import useSort from '@/hooks/useSort';
 import ProductCardSkeleton from '../components/skeletons/ProductCardSkeleton';
 import { productsApi } from '@/api';
-import { useQuery } from '@tanstack/react-query';
 import { ProductData } from '@/api/type';
 import { handleApiError } from '@/utils/handleApiError';
+import { useParams } from 'react-router-dom';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 const CategoryPage = () => {
   const { majorCategory, middleCategory, subCategory } = useParams();
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
 
   const parseCategoryId = (param: string | undefined): number | null => {
     if (!param) return null;
@@ -17,9 +23,9 @@ const CategoryPage = () => {
     return isNaN(id) ? null : id;
   };
 
-  const majorCategoryId = parseCategoryId(majorCategory);
-  const middleCategoryId = parseCategoryId(middleCategory);
-  const subCategoryId = parseCategoryId(subCategory);
+  const majorCategoryId = parseCategoryId(majorCategory as string);
+  const middleCategoryId = parseCategoryId(middleCategory as string);
+  const subCategoryId = parseCategoryId(subCategory as string);
 
   if (majorCategoryId === null) {
     return (
@@ -50,18 +56,15 @@ const CategoryPage = () => {
 
   const { currentSort, currentOrder, sortResult, setCurrentSort } = useSort();
 
-  const {
-    data: categoryProductData,
-    isPending,
-    isError,
-    error,
-  } = useQuery<ProductData[]>({
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isPending, isError, error } = useInfiniteQuery<
+    ProductData[]
+  >({
     queryKey: ['categoryProducts', sortResult, currentOrder, currentCategory],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       try {
         const response = await productsApi.getProductsData({
-          page: 1,
-          pageSize: 20,
+          page: pageParam as number,
+          pageSize: 8,
           sort: sortResult,
           order: currentOrder,
           categoryId: currentCategory,
@@ -71,41 +74,62 @@ const CategoryPage = () => {
         handleApiError(err);
       }
     },
-    staleTime: 5 * 60 * 1000, // 5분 동안 최신으로 간주
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length === 8 ? allPages.length + 1 : undefined;
+    },
+    staleTime: 1000 * 5 * 60,
   });
 
-  if (categoryProductData?.length === 0) {
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage]);
+
+  const categoryProductData = data?.pages.flat();
+
+  if (isError) {
     return (
-      <div className='flex h-screen w-full items-center justify-center'>
-        <span className='text-lg'>존재하지 않는 카테고리이거나 상품이 없어요.</span>
+      <div className='flex h-screen w-full items-center justify-center text-2xl text-primary'>
+        <p>{(error as Error).message}</p>
       </div>
     );
   }
 
-  console.log(categoryProductData);
+  if (categoryProductData?.length === 0) {
+    return (
+      <div className='flex h-screen w-full items-center justify-center'>
+        <span className='text-lg'>상품이 없어요.</span>
+      </div>
+    );
+  }
 
   return (
-    <article className='container mx-auto px-4 py-[160px] transition-all duration-300 ease-in-out'>
-      {isError && (
-        <div className='flex h-screen w-full items-center justify-center text-2xl text-primary'>
-          <p>{error.message}</p>
-        </div>
-      )}
+    <article className={`container mx-auto h-full px-4 pt-[160px] transition-all duration-300 ease-in-out`}>
       <div className='flex w-full flex-col gap-[24px]'>
-        {<SortDropdown currentSort={currentSort} setCurrentSort={setCurrentSort} sortResult={sortResult} />}
+        <SortDropdown currentSort={currentSort} setCurrentSort={setCurrentSort} sortResult={sortResult} />
         <ul className='grid h-full w-full grid-cols-1 gap-6 transition-all duration-300 ease-in-out sm:grid-cols-2 lg:grid-cols-4'>
           {isPending && <ProductCardSkeleton />}
           {categoryProductData &&
-            categoryProductData?.map((item) => (
+            categoryProductData.map((item) => (
               <li key={item.product.id}>
                 <ProductCard
                   productData={item.product}
-                  optionData={item?.options?.[0]}
+                  optionData={item.options[0]}
                   queryKey={['categoryProducts', sortResult, currentOrder, currentCategory]}
                 />
               </li>
             ))}
         </ul>
+        {isFetchingNextPage && (
+          <div className='grid h-full w-full grid-cols-1 gap-6 transition-all duration-300 ease-in-out sm:grid-cols-2 lg:grid-cols-4'>
+            <ProductCardSkeleton />
+          </div>
+        )}
+      </div>
+      <div ref={ref} className='mt-[160px] flex h-[20px] w-full items-center justify-center'>
+        {(isFetchingNextPage || hasNextPage) && <LoadingSpinner size='s' />}
       </div>
     </article>
   );
