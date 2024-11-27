d67f0bb208dc954d49b3a6eb34d92daeea025d21
shin-minhyuk <afs_style@naver.com>
2024-11-27 22:36:09 +0900

    fix: 아이디 번경 부분 훅폼 마이그레이션

--- src/pages/auth/FindPwPage.tsx
+++ src/pages/auth/FindPwPage.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import comepleteCheck from '@/assets/icons/completeCheck.svg';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/Input';
 
 interface UserInfo {
   name: string | null;
   email: string | null;
   phone: number | null;
-  verifyCode: number | null;
+  verification: number | null;
 }
 
 const FindPwPage = () => {
@@ -15,10 +17,16 @@ const FindPwPage = () => {
     name: null,
     email: null,
     phone: null,
-    verifyCode: null,
+    verification: null,
   });
   const [newPw, setNewPw] = useState<string | null>(null);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserInfo>();
 
   const handleFindPwClick = async () => {
     // try {
@@ -26,7 +34,7 @@ const FindPwPage = () => {
     //   const response = await client.post('/api/auth/findPw', {
     //     name: userInfo.name,
     //     email: userInfo.email,
-    //     number: userInfo.verifyCode,
+    //     number: userInfo.verification,
     //   });
 
     //   if (response.status === 200) {
@@ -61,6 +69,9 @@ const FindPwPage = () => {
   const handleVerifyCodeClick = () => {
     // TODO: 휴대폰 인증 번호 전송 로직
   };
+  const handlePwChangeClick = (data: any) => {
+    console.log(data);
+  };
 
   if (isSuccess) {
     return (
@@ -88,44 +99,77 @@ const FindPwPage = () => {
       {/* section 2 */}
       <div>
         {isVerified === false && (
-          <form className='flex flex-col gap-[64px]'>
+          <form onSubmit={handleSubmit(handlePwChangeClick)} className='flex flex-col gap-[64px]'>
             <div className='flex flex-col gap-[10px]'>
               <div className='text-2xl'>회원 정보</div>
-              <input
-                className='w-full border border-gray100 px-6 py-4 placeholder:text-2xl'
+              <Input
+                label='이름'
+                name='name'
                 type='text'
-                placeholder='이름'
-                value={userInfo.name || ''}
-                onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
+                register={register}
+                registerOptions={{
+                  required: '이름은 필수 입력값입니다.',
+                  minLength: {
+                    value: 2,
+                    message: '이름은 최소 2자 이상이어야 합니다.',
+                  },
+                  maxLength: {
+                    value: 10,
+                    message: '이름은 최대 10자까지 입력 가능합니다.',
+                  },
+                  pattern: {
+                    value: /^[가-힣a-zA-Z\s]+$/,
+                    message: '이름은 한글, 영문, 공백만 입력 가능합니다.',
+                  },
+                }}
+                error={errors?.name?.message}
               />
-              <input
-                className='w-full border border-gray100 px-6 py-4 placeholder:text-2xl'
-                type='text'
-                placeholder='이메일'
-                value={userInfo.email || ''}
-                onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
-              />
-              <input
-                className='w-full border border-gray100 px-6 py-4 placeholder:text-2xl'
+              <Input label='이메일' name='email' type='email' register={register} registerOptions={{ required: '' }} />
+              <Input
+                label='전화번호 "-" 없이 입력'
+                name='phone'
                 type='number'
-                placeholder='전화번호 "-" 없이 입력'
-                value={userInfo.phone || ''}
-                onChange={(e) => setUserInfo({ ...userInfo, phone: parseInt(e.target.value) })}
+                register={register}
+                registerOptions={{
+                  required: '전화번호는 필수 입력값입니다.',
+                  minLength: {
+                    value: 11,
+                    message: '전화번호 형식이 올바르지 않습니다.',
+                  },
+                  maxLength: {
+                    value: 11,
+                    message: '전화번호 형식이 올바르지 않습니다.',
+                  },
+                }}
+                error={errors?.phone?.message}
               />
             </div>
 
             <div className='flex flex-col gap-[10px]'>
               <div className='text-2xl'>전화번호 인증</div>
               <div className='flex'>
-                <input
-                  className='w-full border border-gray100 px-6 py-4 placeholder:text-2xl'
+                <Input
+                  label='인증번호'
+                  name='verification'
                   type='number'
-                  placeholder='인증번호'
+                  register={register}
+                  registerOptions={{
+                    required: '인증번호를 입력해주세요.',
+                    minLength: {
+                      value: 6,
+                      message: '인증번호는 6자리여야 합니다.',
+                    },
+                    maxLength: {
+                      value: 6,
+                      message: '인증번호는 6자리여야 합니다.',
+                    },
+                  }}
+                  error={errors?.verification?.message}
                 />
                 <button
                   onClick={() => handleVerifyCodeClick()}
                   type='button'
-                  className='whitespace-nowrap bg-black px-4 py-5 text-white hover:opacity-70'
+                  className='h-[50px] whitespace-nowrap bg-gray100 px-4 py-3 text-gray200'
                 >
                   인증번호 전송
                 </button>
@@ -154,13 +198,13 @@ const FindPwPage = () => {
               <button
                 onClick={() => handleFindPwClick()}
                 type='button'
-                className='bg-black px-4 py-5 text-2xl text-white hover:opacity-70'
+                className='bg-black px-[20px] py-[14px] text-xl text-white hover:opacity-70'
               >
                 비밀번호 찾기
               </button>
               <Link
                 to={'/auth/signin'}
-                className='bg-gray100 px-4 py-5 text-center text-2xl hover:bg-gray400 hover:opacity-70'
+                className='bg-gray100 px-[20px] py-[14px] text-center text-xl hover:bg-gray400 hover:opacity-70'
               >
                 로그인 이동
               </Link>
